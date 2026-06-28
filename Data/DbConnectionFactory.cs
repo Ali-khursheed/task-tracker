0f0a56b55dd784ff3cc4fac69d36c0a41925ccf4
Ali khursheed <alikhursheed911@gmail.com>
2026-06-28 12:01:04 +0500

    Phase 1: Project setup, DB schema, folder structure

---
+++ Data/DbConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+namespace TaskTracker.Data
+{
+  public class DbConnectionFactory
+  {
+    private readonly string _connectionString;
+
+    public DbConnectionFactory(IConfiguration configuaration)
+    {
+      _connectionString=configuaration.GetConnectionString("DefaultConnection")!;
+    }
+
+
+
+    public IDbConnection CreateConnection()
+    {
+      return new SqlConnection(_connectionString);
+    }
+  }
+
+
+
+
+}
+
+
+
